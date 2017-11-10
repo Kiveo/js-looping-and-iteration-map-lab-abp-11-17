5a58e6182cff629ede2b7dbdaa7b7a046ed98c9c
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 22:29:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function nameToAttributes(array1, namestring) {
 function attributesToPhrase(array2) {
   let neoArray = array2.map(function(value) {
     
-    
+    return namespace + " is from " + placespace + ".";
   } ) ;
   debugger;
   return neoArray;

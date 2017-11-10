01ded5dcb6119d0e4d6f4f62d9c259aa8b513f27
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 22:05:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function hydra(head1) {
 */
 // take "First Last" string collection and split into f:"first" l:"last"
 function nameToAttributes(array1, namestring) {
-  let newArray = map(array1, function (head1) {
+  let newArray = array1.map(array1, function (head1) {
     let indexer = head1.indexOf(' '); //get index of space
     let fname = head1.substr(0, indexer); //get first name
     let lname = head1.substr(indexer + 1); //get last name

1b16f3b0f8f2d1bbcc93e49373c9215b90012c1a
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 21:14:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Code your solution in this file.
 
-//callback function for lowercase
-function makeLow(thing) {
-  return thing.toLowerCase();
-}
-
 function lowerCaseDrivers(somearray) {
     //create a new array while lowercasing its elements
     const lowarray = somearray.map(function() {

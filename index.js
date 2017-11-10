519404e2af6984bd8594c88e6ba4a8f2f32e8614
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 22:06:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,21 +9,10 @@ function lowerCaseDrivers(somearray) {
     } ) ;
     return lowarray;
 }
-/*
-// callback for nameToAttributes
-function hydra(head1) {
-  let index = head1.indexOf(" ");  // Get first index where a space occours
-  let fname = head1.substr(0, index); // Get first name
-  let lname = head1.substr(index + 1);  // Get last name
-  let head2 = "firstName: " + fname;
-  let head3 = "lastName: " + lname;
-  //give an attribute
-  head1.assign({}, head2, head3 );
-}
-*/
+
 // take "First Last" string collection and split into f:"first" l:"last"
 function nameToAttributes(array1, namestring) {
-  let newArray = array1.map(array1, function (head1) {
+  let newArray = array1.map(function (head1) {
     let indexer = head1.indexOf(' '); //get index of space
     let fname = head1.substr(0, indexer); //get first name
     let lname = head1.substr(indexer + 1); //get last name
